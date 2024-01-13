87a65055259e715153a9b89540462ec7c4de0149
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2024-01-13 14:09:59 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,82 @@
+import re, time
+from os import environ
+from Script import script 
+
+id_pattern = re.compile(r'^.\d+$')
+
+def is_enabled(value, default):
+    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]: return True
+    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
+    else: return default
+
+
+# PyroClient Setup 
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/45d559dc64f317a2d44d9.jpg https://telegra.ph/file/752d2f177d3aaedbb766b.jpg https://telegra.ph/file/af384f40de32c67f50b3a.jpg https://telegra.ph/file/2d97a771b1e87fa10d3d6.jpg https://telegra.ph/file/e21078f1af9c13fa123ea.jpg https://telegra.ph/file/80de951542c08709fd2eb.jpg https://telegra.ph/file/334bf8623a130a54236cf.jpg https://telegra.ph/file/b1048df4bf8f22433149b.jpg https://telegra.ph/file/a5684977ea3b36ff000de.jpg https://telegra.ph/file/a5684977ea3b36ff000de.jpg https://telegra.ph/file/53d40a7c67ff3f9953b3e.jpg https://telegra.ph/file/56c5453da2be9bb8bf582.jpg https://telegra.ph/file/a8c436b4cceab3850e1dc.jpg https://telegra.ph/file/eabd223b5f3d820cf56a0.jpg https://telegra.ph/file/777166d087c9b32ce7b71.jpg https://telegra.ph/file/4e7ec0052b61374c0cd5d.jpg')).split()
+UPTIME = time.time()
+
+# Admins, Channels & Users
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5745069956').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001666426926').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL','-1001666426926')
+auth_grp = environ.get('AUTH_GROUP')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# MongoDB information
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://trprofilterbot1997:Trproautofilterbot@cluster0.fwfsilk.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
+FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+# Filters Configuration 
+MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
+START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
+BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", script.BUTTON_LOCK_TEXT)
+FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', script.FORCE_SUB_TEXT)
+
+WELCOM_PIC = environ.get("WELCOM_PIC", "https://telegra.ph/file/a931dd99d32486a3b74bf.jpg")
+WELCOM_TEXT = environ.get("WELCOM_TEXT", script.WELCOM_TEXT)
+PMFILTER = is_enabled(environ.get('PMFILTER', "True"), True)
+G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
+BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
+RemoveBG_API = environ.get("RemoveBG_API", "")
+
+# url shortner
+SHORT_URL = environ.get("SHORT_URL")
+SHORT_API = environ.get("SHORT_API")
+
+# Others
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
+MSG_ALRT = environ.get('MSG_ALRT', '✨ Hello Nanbha and Nanbis ❤️')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001685721182))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Discussion_tr_links')
+P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
+PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
+IMDB = is_enabled(environ.get('IMDB', "True"), True)
+SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
+PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
+LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
+
+
+
+
+
