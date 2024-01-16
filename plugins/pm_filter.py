1ca1b0c60fddcf2c0c0d1668b1be943a0f7da4a3
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2024-01-17 03:41:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,19 +39,19 @@ async def pm_next_page(bot, query):
     
     if SHORT_URL and SHORT_API:          
         if SINGLE_BUTTON:
-            btn = [[InlineKeyboardButton(text=f"🎬 [{get_size(file.file_size)}] ➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"🎬[𝖳𝗋™] [{get_size(file.file_size)}] ➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
           
         else:
             btn = [[InlineKeyboardButton(text=f"➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")),
-                    InlineKeyboardButton(text=f"🎬 {get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
+                    InlineKeyboardButton(text=f"🎬[𝖳𝗋™] {get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
             
     else:        
         if SINGLE_BUTTON:
-            btn = [[InlineKeyboardButton(text=f"🎬 [{get_size(file.file_size)}] ➳ {file.file_name}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"🎬[𝖳𝗋™] [{get_size(file.file_size)}] ➳ {file.file_name}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
             
         else:
             btn = [[InlineKeyboardButton(text=f"➳ {file.file_name}", callback_data=f'pmfile#{file.file_id}'),
-                    InlineKeyboardButton(text=f"🎬 {get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
+                    InlineKeyboardButton(text=f"🎬[𝖳𝗋™] {get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
             
 
     btn.insert(0, [InlineKeyboardButton(f"🎬 {search} 🎬", callback_data="🔍❤️")])
@@ -128,19 +128,19 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
 
     if SHORT_URL and SHORT_API:          
         if SINGLE_BUTTON:
-            btn = [[InlineKeyboardButton(text=f"🎬 [{get_size(file.file_size)}] ➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}"))] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"🎬[𝖳𝗋™] [{get_size(file.file_size)}] ➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}"))] for file in files ]
             
         else:
             btn = [[InlineKeyboardButton(text=f"➳ {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}")),
-                    InlineKeyboardButton(text=f"🎬 {get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}"))] for file in files ]
+                    InlineKeyboardButton(text=f"🎬[𝖳𝗋™] {get_size(file.file_size)}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}"))] for file in files ]
             
     else:        
         if SINGLE_BUTTON:
-            btn = [[InlineKeyboardButton(text=f"🎬 [{get_size(file.file_size)}] ➳ {file.file_name}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
+            btn = [[InlineKeyboardButton(text=f"🎬[𝖳𝗋™] [{get_size(file.file_size)}] ➳ {file.file_name}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
             
         else:
             btn = [[InlineKeyboardButton(text=f"➳ {file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
-                    InlineKeyboardButton(text=f"🎬 {get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
+                    InlineKeyboardButton(text=f"🎬[𝖳𝗋™] {get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
             
 
     btn.insert(0, [InlineKeyboardButton(f"🎬 {search} 🎬", callback_data="🔍❤️")])
